58fbab14fb2db6f75975dc9c6e78ef4f90abc859
Satya <Satya@SAT32>
2020-02-08 23:41:11 +0530

    First Commit

---
+++ src/in/nit/emp.java
@@ -0,0 +1,18 @@
+package in.nit;
+
+public class emp {
+	private int empId;
+	private String empName;
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+}
