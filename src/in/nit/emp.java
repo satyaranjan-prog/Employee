ab98a4722802e76765032d8401813624c81279bc
Satya <Satya@SAT32>
2020-02-08 23:59:30 +0530

    Modify

--- src/in/nit/emp.java
+++ src/in/nit/emp.java
@@ -1,14 +1,10 @@
 package in.nit;
 
 public class emp {
-	private int empId;
+	
 	private String empName;
-	public int getEmpId() {
-		return empId;
-	}
-	public void setEmpId(int empId) {
-		this.empId = empId;
-	}
+	
+	
 	public String getEmpName() {
 		return empName;
 	}
